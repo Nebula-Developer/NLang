c10d73ea1a867e262e3cc3a4f850b644611ff555
Nico Cook <nebuladev.contact@gmail.com>
2022-09-24 21:39:38 +1200

    Added new project argument(s)

--- Program.cs
+++ Program.cs
@@ -19,6 +19,11 @@ public static class Program {
 
         void fail() { Environment.Exit(1); return; }
 
+        if (args.Contains("-n") || args.Contains("--new")) {
+            New(args);
+            return;
+        }
+
         if (args.Contains("--help") || args.Contains("-h")) {
             Console.WriteLine("Usage: nlang [options] <input file>\n");
             Console.WriteLine("Example:");
@@ -29,11 +34,10 @@ public static class Program {
             Console.WriteLine("-cc|--ccompile              Compile to a C file");
             Console.WriteLine("-g|--gcc <path>             Specify GCC path");
             Console.WriteLine("-d|--debug                  Enable debug mode");
+            Console.WriteLine("-n|--new                    Create a new project (nlang -n -h for more information)");
             Environment.Exit(0);
             return;
         }
-
-        
 
         for (int i = 0; i < args.Length; i++) {
             String arg = args[i];
@@ -113,6 +117,120 @@ public static class Program {
             Console.WriteLine($"Compile time: {compile.ElapsedMilliseconds}ms");
             Console.WriteLine($"Run time: {run.ElapsedMilliseconds}ms");
             Console.WriteLine($"Total time: {main.ElapsedMilliseconds}ms");
+        }
+    }
+
+    public static void New(String[] args) {
+        if (args.Contains("-h") || args.Contains("--help")) {
+            Console.WriteLine("Usage: nlang [options]\n");
+            Console.WriteLine("Options:");
+            Console.WriteLine("-h|--help                   Show this help message");
+            Console.WriteLine("-p|--project <name>         Create a new project");
+            Console.WriteLine("-cd|--createdir             Create a new directory for the project");
+            Console.WriteLine("-m|--makefile               Create a new Makefile for compiling the project");
+            Console.WriteLine("-i|--ignore                 Create a new .gitignore file for the project");
+            Environment.Exit(0);
+            return;
+        }
+
+        String? name = null;
+        bool isProject = false;
+        bool createNewDir = args.Contains("-cd") || args.Contains("--createdir");
+
+        for (int i = 0; i < args.Length; i++) {
+            String arg = args[i];
+
+            bool hasArgs(params string[] arglist) {
+                if (arglist.Contains(arg.ToLower())) return true;
+                return false;
+            }
+
+            bool nextArgExists = args.Length > i + 1;
+
+            if (hasArgs("-p", "--project")) {
+                if (!nextArgExists) {
+                    Console.WriteLine("Error: No project name specified.");
+                    Environment.Exit(1);
+                    return;
+                }
+
+                name = args[i + 1];
+                isProject = true;
+                i++;
+            }
+        }
+
+        if (name == null) {
+            Console.WriteLine("Error: No project name specified.\nUse -n -h for more information.");
+            Environment.Exit(1);
+            return;
+        }
+
+        if (isProject) {
+            if (createNewDir) {
+                if (Directory.Exists(name)) {
+                    Console.WriteLine("Error: Project already exists.");
+                    Environment.Exit(1);
+                    return;
+                }
+
+                Directory.CreateDirectory(name);
+            }
+
+            String path = createNewDir ? $"{name}/" : "";
+            
+            String[] data = new String[] {
+                "cimport stdio.h;",
+                "",
+                "func main(int argc, (arr)string argv) -> int {",
+                "    printf(\"Hello, world!\");",
+                "    return 0;",
+                "}"
+            };
+
+            String entry = $"{path}{name}.nl";
+
+            if (File.Exists(entry)) {
+                Console.WriteLine("Error: Entry file already exists.");
+                Environment.Exit(1);
+                return;
+            }
+
+            File.WriteAllLines(entry, data);
+
+            if (args.Contains("-m") || args.Contains("--makefile")) {
+                if (File.Exists($"{path}Makefile")) {
+                    Console.WriteLine("Warning: Makefile already exists, ignoring.");
+                } else {
+                    String[] makefile = new String[] {
+                        $"NAME={name}",
+                        $"NLANG=nlang",
+                        "",
+                        $"all: {name}",
+                        "",
+                        $"{name}:",
+                        $"\t$(NLANG) -o $(NAME) $(NAME).nl"
+                    };
+
+                    File.WriteAllLines($"{path}Makefile", makefile);
+                }
+            }
+
+            if (args.Contains("-i") || args.Contains("--ignore")) {
+                if (File.Exists($"{path}.gitignore")) {
+                    Console.WriteLine("Warning: .gitignore already exists, ignoring.");
+                    Environment.Exit(1);
+                    return;
+                } else {
+                    File.WriteAllText($"{path}.gitignore", "nlbin/");
+                }
+            }
+        }
+
+        else {
+            Console.WriteLine("Error: No project type specified.\nUse -n -h for more information.");
+            Environment.Exit(1);
+            return;
         }
     }
 }
